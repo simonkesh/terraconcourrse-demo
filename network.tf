35dd87d7a73d548537a4a1a9bdab0e810ca0c314
simonkesh <simonmcbook@gmail.com>
2023-06-25 00:03:43 +0100

    testing

--- network.tf
+++ network.tf
@@ -2,12 +2,12 @@
 # PROVIDERS
 ##################################################################################
 
-provider "aws" {
+# provider "aws" {
 
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-    region     = var.aws_region
-}
+#     access_key = var.aws_access_key
+#     secret_key = var.aws_secret_key
+#     region     = var.aws_region
+# }
 
 ##################################################################################
 # DATA
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,9 +1,11 @@
-aws_access_key = "AKIAY7J2LW7I4IJOSIMZ"
+aws_access_key = "AKIAW7X2O3VG74MQCZNS"
 
-aws_secret_key = "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"
+aws_secret_key = "VZLFsWkVqGLe19A4SRmHJHAUEdHGidh/FKmYoqhY"
 
 billing_code = "ACCT8675309"
 
 project = "globo-web-app"
+
+aws_region = "us-east-1"
 
 #map_public_ip_on_launch = "34.238.237.151"
